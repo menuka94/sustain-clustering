428b3188cdc83ffce4d4d51c7e0852e248a17c2f
laksheenmendis <cresclux@yahoo.com>
2020-11-03 00:07:38 -0700

    Normalization and getting Correlation Matrix

--- build.sbt
+++ build.sbt
@@ -15,8 +15,16 @@ libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "2.2.1" % "provided",
   "org.apache.spark" %% "spark-sql" % "2.1.3",
   "org.apache.commons" % "commons-math3" % "3.2",
-  "com.github.scopt" %% "scopt" % "3.7.0"
+  "com.github.scopt" %% "scopt" % "3.7.0",
+    // https://mvnrepository.com/artifact/org.apache.spark/spark-mllib
+  "org.apache.spark" %% "spark-mllib" % "2.2.1"
+
 )
+
+//lazy val root = (project in file(".")).dependsOn(sustainDHT).settings()
+
+//lazy val playJongo = RootProject(uri("https://github.com/bekce/play-jongo.git"))
+//lazy val sustainDHT = RootProject(uri("https://github.com/Project-Sustain/synopsis-dht.git#master"))
 
 assemblyMergeStrategy in assembly := {
   case PathList("META-INF", xs @ _*) => MergeStrategy.discard
--- src/main/scala/org/sustain/clustering/DemoDBScanDriver.scala
+++ src/main/scala/org/sustain/clustering/DemoDBScanDriver.scala
@@ -2,6 +2,9 @@ package org.sustain.clustering
 
 import com.mongodb.spark.MongoSpark
 import org.bson.Document
+import org.apache.spark.ml.feature.{MinMaxScaler}
+import org.apache.spark.mllib.linalg.{DenseVector, Matrix, Vectors}
+import org.apache.spark.mllib.stat.Statistics
 
 object DemoDBScanDriver {
 
@@ -11,6 +14,8 @@ object DemoDBScanDriver {
          * parse args for the values to set.
          */
     import org.apache.spark.sql.SparkSession
+
+    case class GeoSpatial(features:DenseVector.type)
 
     val spark = SparkSession.builder()
       .master("spark://olympia:31865")
@@ -26,14 +31,68 @@ object DemoDBScanDriver {
     //df.show()
 
     val rdd = MongoSpark.load(sc)
+//    val new_rdd = rdd.map(doc => doc.get("data").asInstanceOf[Document]).
+//      map(d=> d.get("Latitude") + "," + d.get("Longitude") + "," + d.get("Temperature") + "," + d.get("Precipitation") + "," + d.get("Humidity"))
+
     val new_rdd = rdd.map(doc => doc.get("data").asInstanceOf[Document]).
-      map(d=> d.get("Latitude") + "," + d.get("Longitude") + "," + d.get("Temperature") + "," + d.get("Precipitation") + "," + d.get("Humidity"))
-    //println(rdd.count)
-    // println(new_rdd.first.toString)
+      map(d=> {
 
+        val arr = Array(
+            toDouble(d.get("Latitude").toString)
+          , toDouble(d.get("Longitude").toString)
+          , toDouble(d.get("Temperature").toString)
+          , toDouble(d.get("Precipitation").toString)
+          , toDouble(d.get("Humidity").toString)
+        )
+        Vectors.dense(arr)
+      })
 
-    //println(rdd.count)
-    println(new_rdd.first)
+    val df = spark.createDataFrame(new_rdd, GeoSpatial.getClass)
+
+    print("Soon after converting to Vectors")
+    print(df.take(1))
+
+    //create a MinMaxScaler object
+    val feature_scaler = new MinMaxScaler()
+    feature_scaler.setInputCol("features")
+    feature_scaler.setOutputCol("sfeatures")
+
+    // normalize features
+    val smodel = feature_scaler.fit(df)
+    val s_features_df = smodel.transform(df)
+
+    print("After fitting and transforming")
+    print(s_features_df.take(1))
+
+    print("Show 10 items")
+    print(s_features_df.select("features", "sfeatures").take(10))
+
+    val normalized_df = s_features_df.filter("sfeatures")
+
+    val normalized_rdd = normalized_df.rdd.map(row =>
+      {
+        val arr = Array(
+          row.getDouble(0),
+          row.getDouble(1),
+          row.getDouble(2),
+          row.getDouble(3),
+          row.getDouble(4)
+        )
+        Vectors.dense(arr)
+      })
+
+    print("Dataframe to RDD, 1 record")
+    print(normalized_rdd.take(1))
+
+    // calculate the correlation matrix using Pearson's method. Use "spearman" for Spearman's method.
+    // If a method is not specified, Pearson's method will be used by default.
+    val correlMatrix: Matrix = Statistics.corr(normalized_rdd, "pearson")
+
+    correlMatrix.toString()
   }
 
+  // function to convert year and incident_code columns to int
+  def toDouble(value:String) : Double = {
+    value.toDouble
+  }
 }
