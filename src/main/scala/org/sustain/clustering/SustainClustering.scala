1c016c0197536bac881ef0b35edd039770e875fe
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2021-02-07 03:58:33 -0700

    use county_stats for k-means

--- src/main/scala/org/sustain/clustering/SustainClustering.scala
+++ src/main/scala/org/sustain/clustering/SustainClustering.scala
@@ -1,8 +1,8 @@
 package org.sustain.clustering
 
 import com.mongodb.spark.MongoSpark
-import org.apache.spark.mllib.linalg.DenseVector
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.ml.feature.VectorAssembler
 
 import java.io._
 import java.time.LocalDateTime
@@ -23,18 +23,11 @@ object SustainClustering {
     println("Starting ...")
 
     // TODO: populate the following with user inputs
-    val collection1 = "hospitals_geo"
-    val collection2 = "public_schools"
+    val collection1 = "county_stats"
 
     //    val features1 = Array("properties.POPULATION", "properties.BEDS")
-    val features1 = Array("BEDS", "POPULATION")
-    val features2 = Array("properties.ENROLLMENT", "properties.FT_TEACHER")
+    val features1 = Array("total_population", "median_household_income")
 
-    log("Collections: " + "[" + collection1 + ", " + collection2 + "]")
-    log("Features: [" + features1 + ", " + features2 + "]")
-
-    //    case class GeoSpatial(features: DenseVector.type)
-    case class Collection1(features: DenseVector.type)
 
     val spark = SparkSession.builder()
       .master("spark://menuka-HP:7077")
@@ -44,66 +37,27 @@ object SustainClustering {
 
     val sc = spark.sparkContext
 
+    import com.mongodb.spark.config._
     import spark.implicits._
-    log("Fetching " + collection1 + " ...")
 
-    var df1: DataFrame = MongoSpark.load(spark)
-    df1 = df1.select($"_id", $"properties"("POPULATION"), $"properties"("BEDS"))
-      .withColumnRenamed("properties.BEDS", "BEDS")
-      .withColumnRenamed("properties.POPULATION", "POPULATION")
+    var county_stats = MongoSpark.load(spark,
+      ReadConfig(Map("collection" -> "county_stats", "readPreference.name" -> "secondaryPreferred"), Some(ReadConfig(sc))))
 
-    //    df1.printSchema()
-    //    df1.show(10)
+    county_stats = county_stats.select($"GISJOIN", $"total_population".cast("double"), $"median_household_income");
+    county_stats.printSchema()
+    county_stats.take(5).foreach(i => log(i.toString()))
 
     // K-Means
-    /*
-val assembler = new VectorAssembler().setInputCols(Array("POPULATION", "BEDS")).setOutputCol("features")
-val featureDf = assembler.transform(df1)
+//        /*
+    val assembler = new VectorAssembler().setInputCols(Array("total_population", "median_household_income")).setOutputCol("features")
+    val featureDf = assembler.transform(county_stats)
 
-val kmeans = new KMeans().setK(2).setSeed(1L)
-val model = kmeans.fit(featureDf)
+    val kmeans = new KMeans().setK(10).setSeed(1L)
+    val model = kmeans.fit(featureDf)
 
-log("Cluster centers ...")
-model.clusterCenters.foreach(println)
-    */
-
-    log("Fetching " + collection2 + " ...")
-
-    import com.mongodb.spark.config._
-
-    var dfTotalPopulation = MongoSpark.load(spark,
-      ReadConfig(Map("collection" -> "tract_total_population", "readPreference.name" -> "secondaryPreferred"), Some(ReadConfig(sc))))
-
-
-    dfTotalPopulation = dfTotalPopulation.select($"GISJOIN", $"2010_total_population")
-      .withColumnRenamed("2010_total_population", "population")
-      .as("total_population").na.drop()
-    //    dfTotalPopulation.take(5).foreach(i => log(i.toString()))
-
-    dfTotalPopulation.show(10)
-
-    var dfMedianIncome = MongoSpark.load(spark,
-      ReadConfig(Map("collection" -> "tract_median_household_income", "readPreference.name" -> "secondaryPreferred"), Some(ReadConfig(sc))))
-
-    dfMedianIncome = dfMedianIncome.select($"GISJOIN", $"2010_median_household_income")
-      .withColumnRenamed("2010_median_household_income", "median_income")
-      .as("median_income").na.drop()
-    //    log(dfMedianIncome.schema.toString())
-    //    dfMedianIncome.take(5).foreach(i => log(i.toString()))
-
-    var dfSvi = MongoSpark.load(spark,
-      ReadConfig(Map("collection" -> "svi_tract_GISJOIN", "readPreference.name" -> "secondaryPreferred"), Some(ReadConfig(sc))))
-      .as("svi")
-
-    dfSvi = dfSvi.select($"GISJOIN", $"RPL_THEMES")
-      .withColumnRenamed("RPL_THEMES", "svi").na.drop()
-    log(dfSvi.schema.toString())
-    //    dfSvi.take(5).foreach(i => log(i.toString()))
-
-    // join dataframes on GISJOIN
-    //    val dfJoin1 = dfTotalPopulation.join(dfMedianIncome, $"total_population.GISJOIN == median_income.GISJOIN")
-    //      .select($"total_population.population" as "population", $"median_income.median_income" as "median_income")
-    //    dfJoin1.take(5).foreach(i => log(i.toString()))
+    log("Cluster centers ...")
+    model.clusterCenters.foreach(println)
+//        */
   }
 
   def log(message: String) {
